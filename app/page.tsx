5810f47edf1ed4fa705e9da574565a0721900ce2
Josh Cirre <joshcirre@gmail.com>
2023-11-07 00:36:15 -0700

    feat: bare bones starter

--- app/page.tsx
+++ app/page.tsx
@@ -1,124 +1,113 @@
-"use client";
-import { useState } from "react";
-import { UploadDropzone } from "@/utils/uploadthing";
-import { SubmitButton } from "@/app/components/submit-button";
-import { sendEmail } from "@/app/actions/sendEmail";
-import toast from "react-hot-toast";
-import { getCatPictures } from "@/app/actions/getCatPictures";
-import { useAutoAnimate } from "@formkit/auto-animate/react";
+import Image from 'next/image'
 
 export default function Home() {
-  const [fileUrls, setFileUrls] = useState<string[]>([]);
-  const [fileNames, setFileNames] = useState<string[]>([]);
-  const [emailTo, setEmailTo] = useState<string>("");
-  const [customMessage, setCustomMessage] = useState<string>("");
-  const [sendCustomMessage, setSendCustomMessage] = useState<boolean>(false);
-  const [parent, enableAnimations] = useAutoAnimate();
-
-  const fetchCatPictures = async () => {
-    const catPictures = await getCatPictures();
-    setFileUrls(
-      catPictures.map((cat: { url: string; name: string }) => cat.url)
-    );
-    setFileNames(
-      catPictures.map((cat: { url: string; name: string }) => cat.name)
-    );
-  };
-
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-24 space-y-10">
-      <button
-        onClick={fetchCatPictures}
-        className="px-4 py-2 text-2xl font-bold text-white transition-all duration-500 ease-in-out transform border-2 border-blue-700 rounded-lg shadow-lg bg-gradient-to-r from-blue-500 to-indigo-500 hover:from-blue-600 hover:to-indigo-600 hover:-translate-y-1"
-      >
-        Get Random Cat Pictures
-      </button>
-      <div className="relative flex justify-center text-sm font-medium leading-6">
-        <span className="text-gray-900 text-xs italic">
-          Or upload your own favorite cat pictures.
-        </span>
-      </div>
-      <UploadDropzone
-        endpoint="imageUploader"
-        onClientUploadComplete={(res) => {
-          // Do something with the response
-          console.log("Files: ", res);
-          // Assuming the response object has a property 'fileUrl'
-          setFileUrls(res?.map((file) => file.url) || []);
-          setFileNames(res?.map((file) => file.name) || []);
-          toast.success("Upload complete. Let's send some cats!");
-        }}
-        onUploadError={(error: Error) => {
-          // Do something with the error.
-          alert(`ERROR! ${error.message}`);
-        }}
-      />
-      <form
-        action={() =>
-          sendEmail(
-            fileUrls,
-            fileNames,
-            emailTo,
-            customMessage,
-            sendCustomMessage
-          )
-        }
-      >
-        {fileNames.length > 0 && (
-          <div
-            className="p-4 text-blue-700 bg-blue-100 border-l-4 border-blue-500"
-            role="alert"
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">app/page.tsx</code>
+        </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <a
+            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
           >
-            <p className="font-bold">
-              {fileNames.length} gorgeous cats ready to send.
-            </p>
-          </div>
-        )}
-        <h3 className="mt-4 text-lg font-bold tracking-tight text-blue-900">
-          Who should we send these beautiful kitties to?
-        </h3>
-        <div ref={parent}>
-          <div className="flex items-center h-6 mt-4">
-            <input
-              id="sendCustomMessage"
-              aria-describedby="sendCustomMessage-description"
-              name="sendCustomMessage"
-              type="checkbox"
-              checked={sendCustomMessage}
-              onChange={(e) => setSendCustomMessage(e.target.checked)}
-              className="w-4 h-4 text-indigo-600 border-gray-300 rounded focus:ring-indigo-600"
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className="dark:invert"
+              width={100}
+              height={24}
+              priority
             />
-            <div className="ml-3 text-sm leading-6">
-              <label
-                htmlFor="sendCustomMessage"
-                className="font-medium text-gray-900"
-              >
-                Send custom message
-              </label>
-            </div>
-          </div>
-          {sendCustomMessage && (
-            <textarea
-              value={customMessage}
-              onChange={(e) => setCustomMessage(e.target.value)}
-              required={sendCustomMessage}
-              placeholder="Your custom message"
-              className="w-full px-3 py-2 mt-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
-            />
-          )}
+          </a>
         </div>
-        <div className="flex mt-2 space-x-2">
-          <input
-            type="email"
-            value={emailTo}
-            onChange={(e) => setEmailTo(e.target.value)}
-            placeholder="Recipient's email"
-            required
-            className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
-          />
-          <SubmitButton />
-        </div>
-      </form>
+      </div>
+
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Docs{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Find in-depth information about Next.js features and API.
+          </p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Learn{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Templates{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Explore the Next.js 13 playground.
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Deploy{' '}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
     </main>
-  );
+  )
 }
